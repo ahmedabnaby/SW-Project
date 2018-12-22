210211f7554dd33db49fb80fbaebb1eb08a76c6d
EmanHamzawy <45398790+EmanHamzawy@users.noreply.github.com>
2018-12-22 17:30:09 +0200

    about and create pages

---
+++ about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<body style=" background-image: url('{{asset('images/contactus.jpg')}}');min-height: 100%;min-width: 1024px;width: 100%; height: auto;position: fixed;top: 0;left: 0;background-repeat: no-repeat; background-size: cover;color:white;">
+            <h1>Feel free to contact us!</h1>
+            <h2>We are the website builders</h2> <h3>Your feedback matters to us.</h3>
+            <h4>E-mails:<br> <span style="color:chocolate;">ahmedabnaby.97@gmail.com</span>
+                <br><span style="color:chocolate;">Shabana7052@yahoo.com</span><br>
+        <span style="color:chocolate;">Mostafa1598753@yahoo.com</span><br>
+        <span style="color:chocolate;">Emy.hamzawy753@gmail.com</span><br>  
+        <span style="color:chocolate;">eslamelgeballiii@gmail.com</span><br>  
+</h4>
+        </body>
+
+@endsection
